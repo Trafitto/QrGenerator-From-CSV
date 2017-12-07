2dc906ab0d783310f9b3e3413fee05c69e32ceff
Marco Borchi <borchimarco@gmail.com>
2017-12-07 11:58:04 +0100

    Creating sub dirs for csv with csv filename

--- QrGenerator-csv.py
+++ QrGenerator-csv.py
@@ -5,18 +5,22 @@ import os
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = os.listdir(path_to_dir)
-    return [ path_to_dir+filename for filename in filenames if filename.endswith( suffix ) ]
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
 
 
-
-filenames = find_csv_filenames('Upload/')
+FileCsvDir='Upload/'
+filenames = find_csv_filenames(FileCsvDir)
 
 print ('Start creating qr-image')
 for fname in filenames:
-	csv=open(fname,'r')
-	
+	csv=open(FileCsvDir+fname,'r')
+	fileDir=fname.split('.')
+	imgQrDir='Image/'+fileDir[0]+'/'
+	print('Savng on: ' + imgQrDir)
 	for line in csv:
 		col=line.split(';')
 		out=Create(col[0])
-		out.save('Image/'+col[0]+'.jpg')
+		if not os.path.exists(imgQrDir):
+			os.makedirs('Image/'+fileDir[0])
+		out.save(imgQrDir+col[0]+'.jpg')
 print('End')
